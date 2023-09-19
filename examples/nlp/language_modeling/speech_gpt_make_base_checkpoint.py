72b72266f8347c80b4b72e8a0ef1e64b794e10a8
Paarth Neekhara <paarth.n@gmail.com>
2023-09-19 19:21:40 -0400

    make base checkpoint related changes
    Signed-off-by: Paarth Neekhara <paarth.n@gmail.com>

--- examples/nlp/language_modeling/speech_gpt_make_base_checkpoint.py
+++ examples/nlp/language_modeling/speech_gpt_make_base_checkpoint.py
@@ -14,7 +14,7 @@ from nemo.collections.nlp.parts.nlp_overrides import NLPDDPStrategy, NLPSaveRest
 from nemo.utils import AppState
 
 config = OmegaConf.load(
-    "/home/jasoli/gitrepos/NeMo/examples/nlp/language_modeling/conf/megatron_gpt_prompt_learning_config.yaml"
+    "/home/pneekhara/2023/NeMo/examples/nlp/language_modeling/conf/megatron_gpt_prompt_learning_config.yaml"
 )
 # let's modify some trainer configs
 # check if we have GPU available and uses it
@@ -42,8 +42,8 @@ trainer = pl.Trainer(plugins=plugins, strategy=strategy, **config.trainer)
 print("Trainer config - \n")
 print(OmegaConf.to_yaml(config.trainer))
 
-checkpoint_path = "/home/jasoli/models/gpt_2b_gtc_tp1_pp1_1_1T/megatron_converted_2b_tp1_pp1.nemo"
-# checkpoint_path = "/home/jasoli/models/gpt_843m_gtc_tp1_pp1_1_1T/megatron_converted_843m_tp1_pp1.nemo"
+# checkpoint_path = "/home/jasoli/models/gpt_2b_gtc_tp1_pp1_1_1T/megatron_converted_2b_tp1_pp1.nemo"
+checkpoint_path = "/datap/misc/Checkpoints/megatron_converted_843m_tp1_pp1.nemo"
 # checkpoint_path = "/home/jasoli/models/gpt_pretrain_220m_len_4096_pos_alibi_step_595508_gbs256.nemo"
 gpt_cfg = MegatronGPTModel.restore_from(
     restore_path=checkpoint_path,
@@ -58,8 +58,8 @@ def load_from_checkpoint_dir(cls, cfg, trainer, checkpoint):
     app_state = AppState()
     OmegaConf.resolve(cfg)
     cfg.cfg = cfg
-    cfg.cfg.tokenizer.model = "/home/jasoli/models/gpt_2b_gtc_tp1_pp1_1_1T/2053796188904e679f7e2754a2a1f280_mt_nlg_plus_multilingual_ja_zh_the_stack_frac_015_256k.model"
-    cfg.cfg.tokenizer.tokenizer_model = "/home/jasoli/models/gpt_2b_gtc_tp1_pp1_1_1T/2053796188904e679f7e2754a2a1f280_mt_nlg_plus_multilingual_ja_zh_the_stack_frac_015_256k.model"
+    cfg.cfg.tokenizer.model = "/datap/misc/Checkpoints/gpt843expanded/8c78be8020cb401b9be373a68007f66e_mt_nlg_plus_multilingual_ja_zh_the_stack_frac_015_256k.model"
+    cfg.cfg.tokenizer.tokenizer_model = "/datap/misc/Checkpoints/gpt843expanded/8c78be8020cb401b9be373a68007f66e_mt_nlg_plus_multilingual_ja_zh_the_stack_frac_015_256k.model"
     # cfg.cfg.tokenizer.model = "/home/jasoli/models/gpt_pretrain_220m_len_4096_pos_alibi_step_595508_gbs256/b11cee03bc8940fa86cb2da19e99fd4e_t5_tokenizer.model"
     # cfg.cfg.tokenizer.tokenizer_model = "/home/jasoli/models/gpt_pretrain_220m_len_4096_pos_alibi_step_595508_gbs256/b11cee03bc8940fa86cb2da19e99fd4e_t5_tokenizer.model"
     with tempfile.NamedTemporaryFile(suffix='.yaml') as f:
@@ -85,4 +85,6 @@ print("embedding weight")
 print(model.model.module.language_model.embedding.word_embeddings.weight.shape)
 print("output weight")
 print(model.model.module.language_model.output_layer.weight.shape)
-model.save_to("/home/jasoli/models/speechllm_sgpt_base_2b_tp1_pp1_linearv2.nemo")
+model.save_to("/datap/misc/Checkpoints/converted_megatron_converted_843m_tp1_pp1.nemo")
+
+import ipdb; ipdb.set_trace()
