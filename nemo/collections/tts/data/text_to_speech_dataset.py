d10e2da4decae91dab515d14214a92c53a2e0995
Edresson Casanova <edresson1@gmail.com>
2024-11-21 09:24:25 -0800

    Fix style

--- nemo/collections/tts/data/text_to_speech_dataset.py
+++ nemo/collections/tts/data/text_to_speech_dataset.py
@@ -14,6 +14,7 @@
 
 import json
 import random
+import os
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, Dict, List, Optional
@@ -27,18 +28,18 @@ from nemo.collections.tts.parts.preprocessing.feature_processors import FeatureP
 from nemo.collections.tts.parts.preprocessing.features import Featurizer
 from nemo.collections.tts.parts.utils.tts_dataset_utils import (
     BetaBinomialInterpolator,
+    _read_audio,
     beta_binomial_prior_distribution,
     filter_dataset_by_duration,
     get_weighted_sampler,
     load_audio,
-    _read_audio,
     stack_tensors,
 )
 from nemo.core.classes import Dataset
 from nemo.utils import logging
 from nemo.utils.decorators import experimental
-import os
 import numpy as np
+
 
 @dataclass
 class DatasetMeta:
--- scripts/t5tts/infer_and_evaluate.py
+++ scripts/t5tts/infer_and_evaluate.py
@@ -8,6 +8,18 @@ import soundfile as sf
 import evaluate_generated_audio
 import json
 import argparse
+import glob
+import json
+import os
+import shutil
+
+import evaluate_generated_audio
+import soundfile as sf
+import torch
+from omegaconf.omegaconf import OmegaConf, open_dict
+
+from nemo.collections.tts.data.text_to_speech_dataset import T5TTSDataset
+from nemo.collections.tts.models import T5TTS_Model
 
 dataset_meta_info = {
     'vctk': {
