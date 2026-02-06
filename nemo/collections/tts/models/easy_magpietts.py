53f82f0a220cdad75cc87e8241295656540cc702
Paarth Neekhara <paarth.n@gmail.com>
2026-02-05 21:19:29 -0500

    ignore eval models when loading weights
    Signed-off-by: Paarth Neekhara <paarth.n@gmail.com>

--- nemo/collections/tts/models/easy_magpietts.py
+++ nemo/collections/tts/models/easy_magpietts.py
@@ -545,7 +545,7 @@ class EasyMagpieTTSModel(ModelPT):
         if strict == False:
             super().load_state_dict(state_dict, strict=False)
         for name, child in self.named_children():
-            if name in ['_speaker_verification_model', '_codec_model']:
+            if name in ['_speaker_verification_model', '_codec_model', '_eval_asr_model', '_eval_speaker_verification_model']:
                 continue
             if any(param.numel() > 0 for param in child.parameters()):
                 # If the module has parameters, we want to change the default mapping so that the state_dict gets
