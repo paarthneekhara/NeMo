5186301f380d4b1d15fd7ee3873ebffade673dec
Paarth Neekhara <paarth.n@gmail.com>
2026-03-09 22:21:38 -0400

    Magpie models refactoring
    Signed-off-by: Paarth Neekhara <paarth.n@gmail.com>

--- nemo/collections/tts/models/__init__.py
+++ nemo/collections/tts/models/__init__.py
@@ -16,6 +16,7 @@ from nemo.collections.tts.models.aligner import AlignerModel
 from nemo.collections.tts.models.audio_codec import AudioCodecModel
 from nemo.collections.tts.models.base_magpietts import BaseMagpieTTSModel
 from nemo.collections.tts.models.easy_magpietts import EasyMagpieTTSModel
+from nemo.collections.tts.models.easy_magpietts_inference import EasyMagpieTTSInferenceModel
 from nemo.collections.tts.models.easy_magpietts_preference_optimization import EasyMagpieTTSModelOnlinePO
 from nemo.collections.tts.models.fastpitch import FastPitchModel
 from nemo.collections.tts.models.fastpitch_ssl import FastPitchModel_SSL
@@ -39,6 +40,7 @@ __all__ = [
     "InferBatchOutput",
     "MagpieTTSModel",
     "EasyMagpieTTSModel",
+    "EasyMagpieTTSInferenceModel",
     "EasyMagpieTTSModelOnlinePO",
     "MagpieTTSModelOfflinePODataGen",
     "MagpieTTSModelOfflinePO",
